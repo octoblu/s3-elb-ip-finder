f460c2346461e226075b9ed0955e9431a30f3f81
sqrt <sqrt@octoblu.com>
2015-05-19 11:37:54 -0700

    Add the file

---
+++ get_logs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CWD=`pwd`
+
+aws s3 sync s3://elb-logs.octoblu.com/meshblu/AWSLogs/822069890720/elasticloadbalancing/us-west-2/2015/05/19 logs
+cat logs/*.log  | awk '{print $3}' | sed -e 's/\:.*$//g' | sort | uniq > ips.uniq
+
+NUM_IPS=`wc -l ips.uniq`
+
+echo "wrote ${NUM_IPS} ips to ${CWD}/ips.uniq"
+
